656e57d30ba67a1b93de8326b36a1b689ed7633d
Yon Samphorslita <yon.samphorslita@gmail.com>
2024-10-24 09:46:25 +0700

    TP - Getting started with VueJs
        *Create VueJs project
        *Remove unused code
        *Add message "This is my first VueJs project"

---
+++ e-commerce/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<header>
+  This is my first VueJs project
+</header>
+</template>
+
+<style scoped>
+</style>
