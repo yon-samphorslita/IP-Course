1876551d5415ac92cac7091f1816e401a63e8f4f
yonsamphorslita <yon.samphorslita@gmail.com>
2024-12-05 09:02:45 +0700

    Finished Step 2 in TP4:     - installed vue-router     - created HomeView.vue     - moved code from App.vue to HomeView.vue     - Route to HomeView in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,13 @@
-<template class="App">
+<script setup lang="ts">
+import { RouterView} from 'vue-router';
+console.log('App mounted');
+</script>
+
+<template>
   <div id="app">
-    <AppHeader title="Featured Categories" :nav="groups" />
-    <ProductCategories class="product" :categories = "categories" />
-    <PromotionBanners class="promotion" :banners = "promotions" />
-    <AppHeader title="Popular Products" :nav="groups" />
-    <Product class="products" :products="products"/>
+    <RouterView />
   </div>
 </template>
 
-<script>
-import { useProductStore } from './stores/productStore';
-import ProductCategories from './components/ProductCategories.vue';
-import PromotionBanners from './components/PromotionBanners.vue';
-import AppHeader from './components/menu.vue';
-import Product from './components/Products.vue';
-import { computed, onMounted } from 'vue';
-import { mapState } from 'pinia';
-
-    export default {
-      name: 'App',
-      components: {
-        AppHeader,
-        ProductCategories,
-        PromotionBanners, 
-        Product,
-      },
-      data() {
-        return {
-          currentGroupName: 'All',
-        };
-      },
-      computed: {
-        ...mapState(useProductStore, {
-          popularProducts: 'getPopularProducts',
-        }),
-          categories() {
-            const productStore = useProductStore();
-              return productStore.getCategoriesByGroup(this.currentGroupName)
-            },
-            productsByGroup() {
-              const productStore = useProductStore()
-              return productStore.getProductsByGroup(this.currentGroupName)
-            },
-            productsByCategory() {
-              const productStore = useProductStore()
-              return productStore.getProductsByCategory(this.currentCategoryName)
-            },
-        },
-        setup() {
-          const productStore = useProductStore();
-
-          onMounted(); {
-            productStore.fetchCategories();
-            productStore.fetchGroups();
-            productStore.fetchProducts();
-            productStore.fetchPromotions();
-          };
-
-          const groups = computed(() => productStore.groups);
-          const categories = computed(() => productStore.categories);
-          const promotions = computed(() => productStore.promotions);
-          const popularProducts = computed(() => productStore.getPopularProducts());
-          const products = computed(() => productStore.products);
-
-          return {
-            groups,
-            categories,
-            promotions,
-            popularProducts,
-            products
-          };
-        },
-      };
-</script>
-
-<style>
-#app {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  font-family:Arial, Helvetica, sans-serif;
-  left: 0;
-  right: 0;
-  margin: 0;
-  padding: 10px;
-}
-template {
-  padding: 0px;
-  margin: 0;
-}
+<style scoped>
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
+
+const routes = [ {
+  path: '/',
+  name: 'home',
+  component: HomeView,
+}
+]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  ]
+  history: createWebHistory(),
+  routes,
 })
-
-export default router;
+export default router
